4ba1b0638e73c9da260599582204b509dca4bf55
zhenya1998 <zhenyaylitin@gmail.com>
2018-05-27 21:11:47 +0300

    added tests

---
+++ _build/CMakeFiles/check.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check.dir/tests/main.o"
+  "CMakeFiles/check.dir/tests/test1.o"
+  "check.pdb"
+  "check"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
